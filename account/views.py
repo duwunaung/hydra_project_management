195f09b0bd09627a9f773db51fee39b4042f399d
Pi Rson <duwunaung@gmail.com>
2024-04-12 14:34:01 +0630

    # Setting Project Account Area
    # Add Login and Logout Function
    # init front view

---
+++ account/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from .models import User
+# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        password1 = request.POST.get("password1", "")
+        password2 = request.POST.get("password2", "")
+
+        if password1 == password2 and name and email and password1 and password2:
+            user = User.objects.create_user(name, email, password1)
+            return redirect("/login/")
+        else:
+            print("Something went wrong!")
+    return render(request, "signup.html")
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+
+        if email and password:
+            user = authenticate(request, email=email, password = password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect("/")
+            
+
+    return render(request, "login.html")
